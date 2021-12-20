8966cc951cafafb6c526c7b013d2fe0c7b70fc83
KurumiFake <kurumiisshidowife@gmail.com>
2021-12-20 15:56:38 +0700

    Initial commit 4

---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    `java-library`
+    id("xyz.jpenilla.toothpick")
+}
+
+toothpick {
+    forkName = "MyAirplaneFork"
+    groupId = "com.mygroupid"
+    val versionTag = System.getenv("BUILD_NUMBER")
+        ?: "\"${commitHash() ?: error("Could not obtain git hash")}\""
+    forkVersion = "git-$forkName-$versionTag"
+    forkUrl = "https://github.com/MyName/MyAirplaneFork"
+
+    minecraftVersion = "1.16.5"
+    nmsPackage = "1_16_R3"
+    nmsRevision = "R0.1-SNAPSHOT"
+
+    upstream = "Airplane"
+    upstreamBranch = "origin/master"
+
+    paperclipName = "launcher-myairplanefork"
+
+    server {
+        project = project(":$forkNameLowercase-server")
+        patchesDir = file("patches/server")
+    }
+    api {
+        project = project(":$forkNameLowercase-api")
+        patchesDir = file("patches/api")
+    }
+}
+
+subprojects {
+    repositories {
+        mavenCentral()
+        maven("https://repo.aikar.co/content/groups/aikar/")
+        maven("https://nexus.velocitypowered.com/repository/velocity-artifacts-snapshots/")
+        maven("https://libraries.minecraft.net")
+        maven("https://jitpack.io")
+        mavenLocal()
+    }
+
+    java {
+        sourceCompatibility = JavaVersion.toVersion(8)
+        targetCompatibility = JavaVersion.toVersion(8)
+    }
+}
